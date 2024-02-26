2200c9785e001c9e4e3c78613b9a1657dbb503f8
Nathan <probysuper8@gmail.com>
2024-02-26 16:19:06 -0500

    works with pc not mobile

--- src/components/Home/IntroHeader/introheader.js
+++ src/components/Home/IntroHeader/introheader.js
@@ -5,8 +5,24 @@ import "./introheader.css";
 const IntroHeader = () => {
     return (
         <div className="headerContainer">
-            <h1 className="initial-text">
+            {/* <h1 className="initial-text">
                 Hi, I'm <span className="colour">Nathan Probert</span> 👋.
+            </h1> */}
+            <h1 className='initial-text'>
+                {[[..."Hi,"], [..."I'm"], [..."Nathan"], [..."Probert"]].map((word, i) => {
+                return (
+                    <div key={i}>
+                    {word.map((letter, j) => (
+                        <span key={j} onMouseOver={({ target }) => target.classList.add('jump')} onAnimationEnd={({ target }) => target.classList.remove('jump')}
+                        className="eachChar">
+                        {letter}
+                        </span>
+                    ))}
+              &nbsp;
+
+                    </div>
+                )
+                })}
             </h1>
         </div>
     );
